2561f48573438c00705c99a8c8b9cba18ae25af4
JhonatanZabalaBarriga <zabalaba12@gmail.com>
2022-07-20 09:33:54 -0500

    start project

--- tafa/settings.py
+++ tafa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v@spf9u3vk8vq+rl2&kics=l=-=iaz$9sjzrjkgk=(qhu!nvtk'
+SECRET_KEY = 'SET-ME-PLEASE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
