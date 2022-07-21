44ea4fae0eeeea9439c9568a8d880b0c3dddadca
JhonatanZabalaBarriga <zabalaba12@gmail.com>
2022-07-21 00:34:01 -0500

    add container and temperature_reading models

--- shipments/admin.py
+++ shipments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Container, Temperature_Reading
+
+admin.site.register(Container)
+admin.site.register(Temperature_Reading)
--- shipments/models.py
+++ shipments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Container Model
+# -> ASSET_ID : Primary key
+class Container(models.Model):
+    asset_id = models.CharField(primary_key=True, max_length=250)
+    produce = models.CharField(max_length=250)
+    weight = models.PositiveIntegerField(blank=False, null=False)
+    pick_up_time = models.DateTimeField()
+    delivery_time = models.DateTimeField()
+    ideal_temperature = models.IntegerField()
+    farmer = models.ForeignKey(to=User, on_delete=models.deletion.CASCADE)
+
+    def __str__(self):
+        return self.asset_id
+
+class Temperature_Reading(models.Model):
+    temperature = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
